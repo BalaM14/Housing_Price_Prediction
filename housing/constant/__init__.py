10d64ca5bbd7fe432f95e11baa50632d07c3d51a
BalaM14 <balamurugan141196@gmail.com>
2024-03-10 22:48:40 +0530

    housing/config/configuration.py updated

---
+++ housing/constant/__init__.py
@@ -0,0 +1,32 @@
+import os
+from datetime import datetime
+
+ROOT_DIR=os.getcwd()
+CONFIG_DIR="config"
+CONFIG_FILE_NAME="config.yaml"
+CONFIG_FILE_PATH=os.path.join(ROOT_DIR,CONFIG_DIR,CONFIG_FILE_NAME)
+
+CURRENT_TIME_STAMP=f"{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}"
+
+#Data Ingestion related variable
+
+DATA_INGESTION_CONFIG_KEY="data_ingestion_config"
+DATA_INGESTION_DOWNLOAD_URL_KEY="dataset_download_url"
+DATA_INGESTION_RAW_DATA_DIR_KEY="raw_data_dir"
+DATA_INGESTION_TGZ_DOWNLOAD_DIR_KEY="tgz_download_dir"
+DATA_INGESTION_INGESTED_DIR_NAME_KEY="ingested_dir"
+DATA_INGESTION_TRAIN_DIR_KEY="ingested_train_dir"
+DATA_INGESTION_TEST_DIR_KEY="ingested_test_dir"
+DATA_INGESTION_ARTIFACT_DIR="data_ingestion"
+
+#Data Validation related variable
+DATA_VALIDATION_CONFIG_KEY="data_validation_config"
+DATA_VALIDATION_SCHEMA_DIR="schema_dir"
+DATA_VALIDATION_SCHEMA_FILE_NAME="schema_file_name"
+DATA_VALIDATION_REPORT_FILE_NAME="report_file_name"
+DATA_VALIDATION_REPORT_PAGE_FILE_NAME="report_page_file_name"
+
+#Training Pipeline related variable
+TRAINING_PIPELINE_CONFIG_KEY="training_pipeline_config"
+TRAINING_PIPELINE_ARTIFACT_DIR_KEY="artifact_dir"
+TRAINING_PIPELINE_NAME_KEY="pipeline_name"
--- housing/entity/config_entity.py
+++ housing/entity/config_entity.py
@@ -1,6 +1,11 @@
 from collections import namedtuple
 
-DataIngestionConfig=namedtuple("DataIngestionConfig", ["dataset_download_url","tgz_download_dir","raw_data_dir","ingested_train_dir","ingested_test_dir"])
+DataIngestionConfig=namedtuple("DataIngestionConfig", ["dataset_download_url",
+                                                       "tgz_download_dir",
+                                                       "raw_data_dir",
+                                                       "ingested_train_dir",
+                                                       "ingested_test_dir"])
+
 DataValidationConfig=namedtuple("DataValidationConfig",["Schema_file_path"])
 DataTransformationConfig=namedtuple("DataTransformationConfig",["add_bedroom_per_room",
                                                                 "transformed_train_dir",
@@ -10,3 +15,4 @@ DataTransformationConfig=namedtuple("DataTransformationConfig",["add_bedroom_per
 ModelTrainingConfig=namedtuple("ModelTrainingConfig",["trained_model_file_path","base_accuracy"])
 ModelEvaluationConfig=namedtuple("ModelEvaluationConfig",["model_evaluation_file_path","time_stamp"])
 ModelPusherConfig=namedtuple("ModelPusherConfig",["export_dir_path"])
+TrainingPipelineConfig=namedtuple("TrainingPipelineConfig",["artifact_dir"])
