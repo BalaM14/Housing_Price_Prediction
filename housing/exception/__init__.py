565b7a9e6a93761a8eae863a882e2cb00678d3df
BalaM14 <balamurugan141196@gmail.com>
2024-03-18 00:18:33 +0530

    Schema.yaml file created

--- housing/exception/__init__.py
+++ housing/exception/__init__.py
@@ -17,10 +17,11 @@ class HousingException(Exception):
         '''
 
         _,_,exec_tb=error_detail.exc_info()
-        line_number=exec_tb.tb_frame.f_lineno
+        exception_block_line_number=exec_tb.tb_frame.f_lineno
+        try_block_line_number=exec_tb.tb_lineno
         file_name=exec_tb.tb_frame.f_code.co_filename
-
-        error_message=f"Error occured in scrip : [{file_name}] at line number: [{line_number}] error message: [{error_message}]"
+        
+        error_message=f"Error occured in scrip : [{file_name}] at try block line number: [{try_block_line_number}] and exception block line number: [{exception_block_line_number}] error message: [{error_message}] \n"
         return error_message
     
     def __str__(self):                                  ### its a dunder method
