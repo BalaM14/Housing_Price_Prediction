12e5fc2bb7531c30ce5c8ccad5ea5df8d6d36c80
BalaM14 <balamurugan141196@gmail.com>
2024-06-24 19:57:06 +0530

    model path updated

--- Streamlit_Prediction_API/app.py
+++ Streamlit_Prediction_API/app.py
@@ -2,14 +2,16 @@ import streamlit as st
 import pandas as pd
 import pickle
 import os
+import joblib
 
 from pathlib import Path
 st.text(os.getcwd()) 
 
-latest_model_file_path = "Streamlit_Prediction_API/model.pkl"
+#latest_model_file_path = "Streamlit_Prediction_API/model.pkl"
 
 
-model = pickle.load(open(latest_model_file_path,"rb"))
+#model = pickle.load(open(latest_model_file_path,"rb"))
+model = joblib.load('Streamlit_Prediction_API/model.pkl')
 print(model)
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
