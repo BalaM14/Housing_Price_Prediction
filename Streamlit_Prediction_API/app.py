bc47523d75c524681fe23c1b05813498d8d67695
BalaM14 <balamurugan141196@gmail.com>
2024-06-25 11:38:42 +0530

    Finalized Project Testing

--- Streamlit_Prediction_API/app.py
+++ Streamlit_Prediction_API/app.py
@@ -1,41 +1,95 @@
-import streamlit as st
-import pandas as pd
+# -*- coding: utf-8 -*-
+"""
+Created on 25-06-2024
+
+@author: Bala Murugan
+
+Mail ID: balamurugan141196@gmail.com
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on 25-06-2024
+
+@author: Bala Murugan
+
+Mail ID: balamurugan141196@gmail.com
+"""
+
+import numpy as np
 import pickle
-import os
-import joblib
+import pandas as pd
+#from flasgger import Swagger
+import streamlit as st 
 
-from pathlib import Path
-st.text(os.getcwd()) 
+from PIL import Image
 
-#latest_model_file_path = "Streamlit_Prediction_API/model.pkl"
+#app=Flask(__name__)
+#Swagger(app)
 
-
-#model = pickle.load(open(latest_model_file_path,"rb"))
-model = joblib.load('Streamlit_Prediction_API/model.pkl')
-print(model)
-uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:
-    data = pd.read_csv(uploaded_file)
-    st.write("Input Features:")
-    st.dataframe(data.head())
+pickle_in = open("Streamlit_Prediction_API/model.pkl","rb")
+regressor=pickle.load(pickle_in)
 
 
-    data['Predicted_Price'] = model.predict(data)
+def main():
+    html_temp = """
+    <div style="background-color:tomato;padding:10px">
+    <h2 style="color:white;text-align:center;">Streamlit House Price Predictor ML App </h2>
+    </div>
+    """
+    st.markdown(html_temp,unsafe_allow_html=True)
+    longitude = st.text_input("longitude","")
+    latitude = st.text_input("latitude","")
+    housing_median_age = st.text_input("housing_median_age","")
+    total_rooms = st.text_input("total_rooms","")
+    total_bedrooms = st.text_input("total_bedrooms","")
+    population = st.text_input("population","")
+    households = st.text_input("households","")
+    median_income = st.text_input("median_income","")
+    ocean_proximity = st.selectbox("ocean_proximity",("<1H OCEAN","INLAND","ISLAND","NEAR BAY","NEAR OCEAN"))
+    result=""
 
-    st.write("Output Predicted Features:")
-    st.dataframe(data.head())
+    input_data = {"longitude": [longitude], "latitude": [latitude], "housing_median_age": [housing_median_age],
+                  "total_rooms": [total_rooms], "total_bedrooms": [total_bedrooms], "population":[population],
+                  "households": [households], "median_income":[median_income], "ocean_proximity": [ocean_proximity]}
+    
+    dataframe = pd.DataFrame(input_data)
 
-    @st.cache_data
-    def convert_df(df):
-        return df.to_csv(index=False).encode('utf-8')
+    if st.button("Predict"):
+        result=regressor.predict(dataframe)
+    st.success('The output is {}'.format(result))
+
+    st.write("")
+    st.write(f"{'=='*17}   BATCH PREDICTION   {'=='*18}")
+    uploaded_file = st.file_uploader("Choose a file")
+
+    if uploaded_file is not None:
+        data = pd.read_csv(uploaded_file)
+        st.write("Input Features: limited to 5 rows")
+        st.dataframe(data.head())
+
+        if st.button("Run"):
+            data['Predicted_Price'] = regressor.predict(data)
+            st.write("Output Predicted Features: limited to 5 rows")
+            st.dataframe(data.head())
+
+            @st.cache_data
+            def convert_df(df):
+                return df.to_csv(index=False).encode('utf-8')
 
 
-    csv = convert_df(data)
+            csv = convert_df(data)
 
-    st.download_button(
-    "Press to Download",
-    csv,
-    "file.csv",
-    "text/csv",
-    key='download-csv'
-    )
+            st.download_button(
+            "Download Predicted Output File",
+            csv,
+            "file.csv",
+            "text/csv",
+            key='download-csv'
+            )
+
+if __name__=='__main__':
+    main()
+    
+    
+    
